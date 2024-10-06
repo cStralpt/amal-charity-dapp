eaf9e091dd5dc7b29a23688fd4470435066f7aa0
Satri Ali <satrialipatullah@gmail.com>
2024-10-06 22:17:45 +0700

    feat: add charity form

--- app/(tabs)/charity.tsx
+++ app/(tabs)/charity.tsx
@@ -1,29 +1,62 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { Pressable, StyleSheet, Text, TextInput, View } from 'react-native';
 import { Colors } from '@/constants/Colors';
 
 export default function CharityScreen() {
   return (
-    <View style={{ backgroundColor: Colors.main.primaryBlue, flex: 1 }}>
-      <Text>Charity</Text>
+    <View style={styles.container}>
+      <TextInput
+        placeholder='charity program title'
+        style={styles.titleInput} />
+      <TextInput
+        placeholder='charity description'
+        textAlignVertical="top"
+        numberOfLines={4}
+        style={styles.descriptionInput}
+        multiline={true} />
+      <Pressable style={({ pressed }) => [styles.button, pressed && styles.buttonActive]}>
+        <Text style={{
+          color: Colors.main.skyMilk,
+          fontWeight: "700"
+        }}>CREATE CHARITY</Text>
+      </Pressable>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    gap: 8,
+  titleInput: {
+    backgroundColor: Colors.main.skyMilk,
+    color: Colors.main.primaryBlue,
+    padding: 10,
+    borderRadius: 10,
+    width: "100%"
   },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
+  container: {
+    backgroundColor: Colors.main.primaryBlue,
+    flex: 1,
+    paddingTop: 40,
+    paddingHorizontal: 20,
+    gap: 10,
+    alignItems: "center"
   },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
+  descriptionInput: {
+    backgroundColor: Colors.main.skyMilk,
+    color: Colors.main.primaryBlue,
+    padding: 10,
+    borderRadius: 10,
+    width: "100%",
+    flex: 1
   },
+  button: {
+    borderWidth: 1,
+    padding: 10,
+    borderColor: "transparent",
+    borderRadius: 10
+  },
+  buttonActive: {
+    backgroundColor: "rgba(255,255,255,0.2)",
+    borderWidth: 1,
+    borderColor: "rgba(255,255,255,0.5)"
+
+  }
 });
