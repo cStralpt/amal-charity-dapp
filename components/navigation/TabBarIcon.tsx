cdb992eedf88fdcba9f481736b94a9b72255e965
Satri Ali <satrialipatullah@gmail.com>
2024-10-06 17:00:03 +0700

    add: tab bar icon set

--- components/navigation/TabBarIcon.tsx
+++ components/navigation/TabBarIcon.tsx
@@ -1,9 +1,56 @@
-// You can explore the built-in icon families and icons on the web at https://icons.expo.fyi/
+import { Colors } from '@/constants/Colors';
+import { HandHelping, House, NotebookTabs, Wallet } from 'lucide-react-native';
+import { View, StyleSheet } from 'react-native';
 
-import Ionicons from '@expo/vector-icons/Ionicons';
-import { type IconProps } from '@expo/vector-icons/build/createIconSet';
-import { type ComponentProps } from 'react';
+const HomeIcon = ({ isActive }: { isActive: boolean }) => {
+  return (
+    <House
+      fill={isActive ? Colors.main.skyMilk : "none"}
+      size={23}
+      color={isActive ? Colors.main.armyGreen : Colors.main.skyMilk}
+    />
+  );
+};
 
-export function TabBarIcon({ style, ...rest }: IconProps<ComponentProps<typeof Ionicons>['name']>) {
-  return <Ionicons size={28} style={[{ marginBottom: -3 }, style]} {...rest} />;
+const CharityIcon = ({ isActive }: { isActive: boolean }) => {
+  return (
+    <HandHelping
+      fill={isActive ? Colors.main.skyMilk : "none"}
+      size={23}
+      color={isActive ? Colors.main.armyGreen : Colors.main.skyMilk}
+    />
+  );
+};
+
+const WalletIcon = ({ isActive }: { isActive: boolean }) => {
+  return (
+    <Wallet
+      fill={isActive ? Colors.main.skyMilk : "none"}
+      size={23}
+      color={isActive ? Colors.main.armyGreen : Colors.main.skyMilk}
+    />
+  );
+};
+
+export function TabBarIcon({
+  isActive,
+  name,
+}: { name: "home" | "charity" | "wallet"; isActive: boolean }) {
+  return (
+    <View style={styles.icon}>
+      {name === "home" && <HomeIcon isActive={isActive} />}
+      {name === "wallet" && <WalletIcon isActive={isActive} />}
+      {name === "charity" && <CharityIcon isActive={isActive} />}
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  icon: {
+    borderRadius: 99,
+    width: 60,
+    justifyContent: "center",
+    alignItems: "center",
+    paddingVertical: 2,
+  },
+});
